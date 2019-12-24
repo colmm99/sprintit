45dda9171ce07d2cd644af7255d16721e053378f
cmonks <cmonks@archi3.fritz.box>
2019-12-24 00:26:45 +0000

    test change

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -12,7 +12,7 @@ public class GreetingController {
   private final AtomicLong counter = new AtomicLong();
 
   @RequestMapping("/greeting")
-  public Greeting greeting(@RequestParam(value="name", defaultValue="Colm") String name) {
+  public Greeting greeting(@RequestParam(value="name", defaultValue="kolm99") String name) {
     return new Greeting(counter.incrementAndGet(),
               String.format(template, name));
   }
